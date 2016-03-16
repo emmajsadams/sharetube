44c09aa0768d3f2dc7adfa6c3ab603bacc442345
Emma Cline <7471922-EmmaJCline@users.noreply.gitlab.com>
2016-03-15 20:42:38 -0700

    Initial project structure * .gitignore ignoring node_modules and bundle.js * babelrc with react and s2015 * boilerplate index.html to display bundle * package.json with initial plugins

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  entry: {
+    app: './index.js'
+  },
+
+  // Add resolve.extensions.
+  // '' is needed to allow imports without an extension.
+  // Note the .'s before extensions as it will fail to match without!!!
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+
+  output: {
+    // path: PATHS.build,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        // include: PATHS.app
+      },
+      // Set up jsx. This accepts js too thanks to RegExp
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel', // 'babel-loader' is also a legal name to reference
+      }
+    ]
+  }
+};
