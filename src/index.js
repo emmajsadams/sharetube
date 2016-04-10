4b5a23b57c553a2ab5e56b13c9af641337f78659
Emma Cline <7471922-EmmaJCline@users.noreply.gitlab.com>
2016-04-10 14:20:38 -0700

    Use * to match any route

--- src/index.js
+++ src/index.js
@@ -2,19 +2,17 @@ import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom';
 import { Provider } from 'react-redux'
-import { Router, Route, Redirect, browserHistory } from 'react-router'
+import { Router, Route, browserHistory } from 'react-router'
 
 import configureStore from './configureStore'
 import MainContainer from './containers/mainContainer'
 
-const prefix = '/'
 const store = configureStore({})
 
 render((
   <Provider store={store}>
     <Router history={browserHistory}>
-      <Route path={prefix} component={MainContainer} />
-      <Redirect from="/" to={prefix} />
+      <Route path="*" component={MainContainer} />
     </Router>
   </Provider>
 ), document.getElementById('react-mount'))
