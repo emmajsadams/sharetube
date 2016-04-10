b04877c939ee4fb5717356bfc17f902393f13691
Emma Cline <7471922-EmmaJCline@users.noreply.gitlab.com>
2016-04-10 14:10:55 -0700

    Add prefix to build

--- src/index.js
+++ src/index.js
@@ -4,17 +4,19 @@ import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom';
 import { Provider } from 'react-redux'
-import { Router, Route, browserHistory } from 'react-router'
+import { Router, Route, Redirect, browserHistory } from 'react-router'
 
 import configureStore from './configureStore'
 import MainContainer from './containers/mainContainer'
 
+const prefix = process.env.ROUTE_PREFIX || '/'
 const store = configureStore({})
 
 render((
   <Provider store={store}>
     <Router history={browserHistory}>
-      <Route path="/" component={MainContainer} />
+      <Route path={prefix} component={MainContainer} />
+      <Redirect from="/" to={prefix} />
     </Router>
   </Provider>
 ), document.getElementById('react-mount'))
